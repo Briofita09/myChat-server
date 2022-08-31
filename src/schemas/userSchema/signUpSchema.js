1694ebcf9b4a0718d4a1f81f58d7cb553cf4672d
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-08-31 15:10:11 -0300

    feat: created login and signUp schema with joi

---
+++ src/schemas/userSchema/signUpSchema.js
@@ -0,0 +1,8 @@
+import Joi from "joi";
+
+export const UserSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+  confirmPassword: Joi.ref("password"),
+});
